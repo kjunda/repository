4a09d90baae34cbcaa30b897ef351390169d6732
kjunda <54493316+kjunda@users.noreply.github.com>
2023-02-18 17:10:54 -0300

    Update proxy-local.sh

--- proxy-local.sh
+++ proxy-local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /etc/apt/apt.conf.d/
-echo "Acquire::http { Proxy "http://192.168.1.192:3142"; }; > 02proxy"
+echo "Acquire::http { Proxy "http://192.168.1.192:3142"; };" > 02proxy
