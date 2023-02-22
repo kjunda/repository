2a6f6e0832ed0e21b5117a21bd444033df4cd4ee
kjunda <54493316+kjunda@users.noreply.github.com>
2023-02-22 08:52:40 -0300

    Update mirror-update.sh

--- mirror-update.sh
+++ mirror-update.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy/ /var/www/html/ubuntu/dists/jammy
+sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy/ /home/debian/REPO/ubuntu/dists/jammy
 
-sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-updates/ /var/www/html/ubuntu/dists/jammy-updates
+sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-updates/ /home/debian/REPO/ubuntu/dists/jammy-updates
 
-sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-security/ /var/www/html/ubuntu/dists/jammy-security
+sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-security/ /home/debian/REPO/ubuntu/dists/jammy-security
 
-sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-proposed/ /var/www/html/ubuntu/dists/jammy-proposed
+sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-proposed/ /home/debian/REPO/ubuntu/dists/jammy-proposed
 
-sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-backports/ /var/www/html/ubuntu/dists/jammy-backports
+sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/dists/jammy-backports/ /home/debian/REPO/ubuntu/dists/jammy-backports
 
-sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/pool/ /var/www/html/ubuntu/pool
-
-
+sudo rsync --recursive --links --perms --times --compress --progress --delete rsync://archive.ubuntu.com/ubuntu/pool/ /home/debian/REPO/ubuntu/pool
 
 #http://archive.ubuntu.com/ubuntu/dists/jammy/
 #http://archive.ubuntu.com/ubuntu/dists/jammy-updates/
